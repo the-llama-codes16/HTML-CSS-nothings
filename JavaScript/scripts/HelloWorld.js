70fe928b2fe7915abae8d5436bb283cf3c862e6b
the-llama-codes16 <83777103+the-llama-codes16@users.noreply.github.com>
2023-10-02 22:57:43 +0800

    JavaScript practice

---
+++ JavaScript/scripts/HelloWorld.js
@@ -0,0 +1,20 @@
+"use strict";
+// The statement above explicitly enables the script to work the modern way.
+// We can also use that inside functions, so only that function works the modern way.
+// Must always write at the top to work!!
+
+// Must enter the JavaScript world with a proper greeting
+alert("Hello World!")
+
+// Create a page that shows a message "I'm JavaScript!"
+alert("I'm JavaScript!")
+
+// Working with Variables
+let admin;
+let name;
+name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUser = "User 01";
